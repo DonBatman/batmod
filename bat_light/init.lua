de4726b003f754f1a2c4c7af485820e5d92326ea
DonBatman <serfdon@gmail.com>
2014-10-13 10:46:39 -0700

    Updated readme.md

--- bat_light/init.lua
+++ bat_light/init.lua
@@ -1,5 +1,14 @@
-batmod
-======
+dofile(minetest.get_modpath("bat_light").."/bat_lamp_post.lua")
+dofile(minetest.get_modpath("bat_light").."/trafficlight.lua")
+dofile(minetest.get_modpath("bat_light").."/cubes.lua")
+dofile(minetest.get_modpath("bat_light").."/lights.lua")
+print("bat light loaded")
+
+-----------------------------------------------------------------------
+-------------------------Batman Mod------------------------------------
+-----------------------------------------------------------------------
+
+--[[
 Batman Mod is a mod created so I could learn how to make mods. It is a combination of my work and the work of
 many others. I want to thank everyone who works to make Minetest a better game. 
 If I have used your code and did not give credit or if I was not allowed to use the code please let me know.
@@ -15,5 +24,7 @@ The Batman mod is licened under "wtfpl"
       **        ***        **
        **       ***       **
         **       *       **
+]]
+
 
 
