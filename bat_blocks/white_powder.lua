9b3079bbf2afa8e4bb8421de208ec8261633d525
DonBatman <serfdon@gmail.com>
2014-10-13 21:49:00 -0700

    Changed white powder from node to craftitem

--- bat_blocks/white_powder.lua
+++ bat_blocks/white_powder.lua
@@ -1,14 +1,10 @@
 -- Bat White Powder
 
-minetest.register_node("bat_blocks:bat_white_powder", {
+minetest.register_craftitem("bat_blocks:bat_white_powder", {
 	description = "Bat White Powder",
-	drawtype = "plantlike",
-	tile_images = {"bat_white_powder.png"},
 	inventory_image = "bat_white_powder.png",
-	paramtype = "light",
 })
 
-minetest.register_alias("batmod:bat_white_powder", "bat_blocks:bat_white_powder")
 
 --Craft
 
