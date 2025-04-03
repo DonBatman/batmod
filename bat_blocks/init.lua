0352836305e575b85ff3c3cd93b2dc6ce9ca41e2
Don <serfdon@gmail.com>
2025-04-03 01:51:06 -0700

    Update init.lua

--- bat_blocks/init.lua
+++ bat_blocks/init.lua
@@ -22,7 +22,7 @@ Batman Mod is a mod created so I could learn how to make mods. It is a combinati
 many others. I want to thank everyone who works to make Minetest a better game. 
 If I have used your code and did not give credit or if I was not allowed to use the code please let me know.
 I neglected to keep track of whos work I was using. I mean no disrespect. Sorry!
-The Batman mod is licened under "wtfpl"
+The Batman mod is licened under MIT. Media is CC0
 
         **     *   *     **
        **      ** **      **
